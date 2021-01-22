6fb9fb82f0b02efefabcb03e0d9ec8554044df9a
Pavel Simonov <pavel.simonov@citeck.ru>
2021-01-22 16:26:38 +0700

    ECOSCOM-4124 - Add ECOS applications support

--- src/main/java/ru/citeck/ecos/apps/app/application/init/buildinfo/BuildInfoSender.kt
+++ src/main/java/ru/citeck/ecos/apps/app/application/init/buildinfo/BuildInfoSender.kt
@@ -62,7 +62,7 @@ class BuildInfoSender(
         commandsService.execute {
             targetApp = "uiserv"
             body = AddBuildInfoCommand(listOf(
-                    AppBuildInfo("emodel", "ECOS Model", "", info)
+                    AppBuildInfo("eapps", "Applications", "", info)
             ))
             ttl = Duration.ofHours(1)
         }
