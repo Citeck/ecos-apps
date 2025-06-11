eff8b6ad87b5589f306b1220f9a4a1b7fcb1c293
Roman Makarskiy <romanchabest55@gmail.com>
2025-06-11 13:02:54 +0300

    ECOSENT-3361 - Notification templates are not getting deployed

--- src/main/java/ru/citeck/ecos/apps/domain/artifact/patch/UpdateArtifactsExtIdPatch.kt
+++ src/main/java/ru/citeck/ecos/apps/domain/artifact/patch/UpdateArtifactsExtIdPatch.kt
@@ -16,8 +16,13 @@ import ru.citeck.ecos.apps.domain.artifact.artifact.service.EcosArtifactsDao
 import ru.citeck.ecos.apps.domain.artifact.artifact.service.EcosArtifactsService
 import ru.citeck.ecos.apps.domain.artifact.type.service.EcosArtifactTypesService
 import ru.citeck.ecos.apps.eapps.dto.ArtifactUploadDto
+import ru.citeck.ecos.commons.data.ObjectData
+import ru.citeck.ecos.records2.RecordConstants
+import ru.citeck.ecos.records3.RecordsService
+import ru.citeck.ecos.records3.record.atts.dto.RecordAtts
 import ru.citeck.ecos.webapp.api.constants.AppName
 import ru.citeck.ecos.webapp.lib.patch.annotaion.EcosPatch
+import ru.citeck.ecos.webapp.lib.patch.annotaion.EcosPatchDependsOn
 import ru.citeck.ecos.webapp.lib.patch.annotaion.EcosPatchDependsOnApps
 import java.util.concurrent.Callable
 
@@ -124,42 +129,166 @@ class UpdateArtifactsExtIdPatch(
         return "Patched: $patched"
     }
 
-    private fun getAppSourceKeyForEntity(entity: EcosArtifactEntity): AppSourceKey? {
-        val ecosApp = entity.ecosApp ?: ""
-        val sourceType = findSourceTypeForEntity(entity) ?: return null
-        val lastRev = entity.lastRev ?: return null
 
-        return AppSourceKey(
-            ecosApp,
-            SourceKey(
-                lastRev.sourceId ?: "",
-                sourceType
-            )
+}
+
+@Component
+@EcosPatch("fix-notification-artifacts-patch", "2025-06-10T00:00:00Z")
+@EcosPatchDependsOn("update-artifact-ext-ids-patch-3")
+@EcosPatchDependsOnApps(AppName.EMODEL, AppName.NOTIFICATIONS)
+class FixNotificationArtifactsPatch(
+    private val ecosArtifactsService: EcosArtifactsService,
+    private val ecosArtifactsRepo: EcosArtifactsRepo,
+    private val ecosArtifactsDao: EcosArtifactsDao,
+    private val recordsService: RecordsService
+) : Callable<String> {
+
+    companion object {
+        private val log = KotlinLogging.logger {}
+
+        private const val NOTIFICATION_ARTIFACT_TYPE = "notification/template"
+        private const val END_WITH_WRONG_POSTFIX = "html.zip"
+
+        private val getValidIdRegex = Regex(".*/([^/]+)\\.html\\.zip")
+    }
+
+    override fun call(): String {
+
+        var currentSkip = 0
+        val batchSize = 50
+        var patched = 0
+
+        var foundArtifacts = listOf<EcosArtifactDto>()
+        val findArtifacts = fun() {
+            foundArtifacts = ecosArtifactsService.getAllArtifacts(currentSkip, batchSize)
+        }
+
+        findArtifacts()
+
+        while (foundArtifacts.isNotEmpty()) {
+            foundArtifacts.forEach { artifact ->
+
+                val artifactType: String = artifact.type
+                if (artifactType != NOTIFICATION_ARTIFACT_TYPE) {
+                    return@forEach
+                }
+
+                val currentId = artifact.id
+                if (currentId.endsWith(END_WITH_WRONG_POSTFIX).not()) {
+                    return@forEach
+                }
+
+                val matcher = getValidIdRegex.find(currentId)
+                val validId = matcher?.groupValues?.get(1) ?: let {
+                    log.warn { "Invalid artifact id format: $currentId" }
+                    return@forEach
+                }
+
+                log.info { "Processing notification artifact with ext id: $currentId" }
+
+                val artifactEntity = ecosArtifactsRepo.getByExtId(artifactType, currentId) ?: let {
+                    log.info { "Skip not found or deleted artifact. Ext id: $currentId, type: $artifactType" }
+                    return@forEach
+                }
+
+                val artifactWithValidId = ecosArtifactsRepo.getByExtId(artifactType, validId)
+                if (artifactWithValidId != null) {
+                    ecosArtifactsDao.delete(artifactWithValidId)
+                    log.info { "Deleted artifact with ext id: $validId, type: $artifactType" }
+                }
+
+                saveValidIdToEntity(artifactEntity, validId)
+                redeployNotification(validId)
+
+                patched++
+            }
+
+            currentSkip += batchSize
+
+            findArtifacts()
+        }
+
+        log.info {
+            "Patched: $patched artifacts"
+        }
+
+        return "Patched: $patched"
+    }
+
+
+    private fun saveValidIdToEntity(entity: EcosArtifactEntity, validId: String) {
+        val notValidId = entity.extId
+
+        entity.extId = validId
+        ecosArtifactsDao.save(entity)
+        log.info { "Updated artifact ext id: $notValidId -> $validId" }
+    }
+
+    private fun redeployNotification(notificationId: String) {
+        val archiveName = "$notificationId.zip"
+        val notificationRef = "${AppName.NOTIFICATIONS}/template@$notificationId"
+
+        val notificationDataBase64 = recordsService.getAtt(notificationRef, "data").asText()
+        if (notificationDataBase64.isBlank()) {
+            log.error { "Notification data not found for ref: $notificationRef" }
+            return
+        }
+
+        val atts = RecordAtts("${AppName.NOTIFICATIONS}/template@")
+
+        atts[RecordConstants.ATT_WORKSPACE] = "admin\$workspace"
+        atts[".att(n:\"_content\"){as(n:\"content-data\"){json}}"] = listOf(
+            ObjectData.create()
+                .set("storage", "base64")
+                .set("name", archiveName)
+                .set("url", "data:application/zip;base64,$notificationDataBase64")
+                .set("size", notificationDataBase64.length)
+                .set("type", "application/zip")
+                .set("originalName", archiveName)
         )
+
+        val mutateResult = recordsService.mutate(atts)
+
+        log.info { "Mutate result: $mutateResult" }
     }
+}
 
-    private fun findSourceTypeForEntity(entity: EcosArtifactEntity): ArtifactSourceType? {
-        val lastRev = entity.lastRev
-        lastRev.sourceType?.toArtifactSourceType()?.let { return it }
 
-        return findSourceTypeInPreviousRevisions(lastRev.prevRev)
+private fun getAppSourceKeyForEntity(entity: EcosArtifactEntity): AppSourceKey? {
+    val ecosApp = entity.ecosApp ?: ""
+    val sourceType = findSourceTypeForEntity(entity) ?: return null
+    val lastRev = entity.lastRev ?: return null
+
+    return AppSourceKey(
+        ecosApp,
+        SourceKey(
+            lastRev.sourceId ?: "",
+            sourceType
+        )
+    )
+}
+
+private fun findSourceTypeForEntity(entity: EcosArtifactEntity): ArtifactSourceType? {
+    val lastRev = entity.lastRev
+    lastRev.sourceType?.toArtifactSourceType()?.let { return it }
+
+    return findSourceTypeInPreviousRevisions(lastRev.prevRev)
+}
+
+private fun findSourceTypeInPreviousRevisions(revision: EcosArtifactRevEntity?): ArtifactSourceType? {
+    var currentRevision = revision
+    while (currentRevision != null) {
+        currentRevision.sourceType?.toArtifactSourceType()?.let { return it }
+        currentRevision = currentRevision.prevRev
     }
+    return null
+}
 
-    private fun findSourceTypeInPreviousRevisions(revision: EcosArtifactRevEntity?): ArtifactSourceType? {
-        var currentRevision = revision
-        while (currentRevision != null) {
-            currentRevision.sourceType?.toArtifactSourceType()?.let { return it }
-            currentRevision = currentRevision.prevRev
-        }
-        return null
-    }
-
-    private fun ArtifactRevSourceType.toArtifactSourceType(): ArtifactSourceType? {
-        return when (this) {
-            ArtifactRevSourceType.APPLICATION -> ArtifactSourceType.APPLICATION
-            ArtifactRevSourceType.USER -> ArtifactSourceType.USER
-            ArtifactRevSourceType.ECOS_APP -> ArtifactSourceType.ECOS_APP
-            else -> null
-        }
+private fun ArtifactRevSourceType.toArtifactSourceType(): ArtifactSourceType? {
+    return when (this) {
+        ArtifactRevSourceType.APPLICATION -> ArtifactSourceType.APPLICATION
+        ArtifactRevSourceType.USER -> ArtifactSourceType.USER
+        ArtifactRevSourceType.ECOS_APP -> ArtifactSourceType.ECOS_APP
+        else -> null
     }
 }
